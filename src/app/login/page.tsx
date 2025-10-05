e5d75ab8680649849f83add4768575f848c975ba
Jaber Alsalamah <46474223+SWEJaber@users.noreply.github.com>
2025-10-05 16:37:19 +0300

    App header (#2)
    * fix middleware filtering static files
    
    * create basic header

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,11 @@
 export default function Login() {
-    return (<div>Login</div>)
+    return (
+        <div className="h-[calc(100vh-4rem)] flex flex-col justify-center items-center">
+            <p className="text-7xl">Log in</p>
+
+            <div className="h-100 w-70 mt-10 bg-amber-600">
+
+            </div>
+        </div>
+    )
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -6,16 +6,25 @@ export function middleware(request: NextRequest) {
     console.log("request: ", request.nextUrl.pathname);
 
     if (!isAuthenticated) {
-        if (request.nextUrl.pathname === "/login") {
-            return NextResponse.next();
+        if (request.nextUrl.pathname !== "/login") {
+            return NextResponse.redirect(new URL("/login", request.url))
         }
-
-        return NextResponse.redirect(new URL("/login", request.url))
-    } else {
-        if (request.nextUrl.pathname === "/login") {
-            return NextResponse.redirect(new URL("/bot", request.url))
-        }
-    }
+    } else if (request.nextUrl.pathname !== "/bot") {
+        return NextResponse.redirect(new URL("/bot", request.url))
+    }    
 
     return NextResponse.next();
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - All files inside the public folder can also be excluded
+     */
+    '/((?!_next/static|_next/image|favicon.ico|public/|assets/).*)',
+  ],
+};
